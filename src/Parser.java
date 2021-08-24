f604be99712f7f9ab0d19db5638388c796bbdd35
Jason Ng <1995jasonng@gmail.com>
2021-08-24 18:37:17 +0930

    -fix the bug of updating current_char in parse_bracket, before that current_char didn't update when char_index updated

--- src/Parser.java
+++ src/Parser.java
@@ -11,6 +11,7 @@ public class Parser {
 
     ArrayList<ExpNode> grammarTree = new ArrayList<ExpNode> ();
     ArrayList<String> regexSeq = new ArrayList<String> ();
+    ArrayList<ExpNode> tmpStrInBracket = new ArrayList<ExpNode> ();
 
     void next_char()
     {   
@@ -56,25 +57,19 @@ public class Parser {
         return false;
     }
 
-    // create start node
-    void create_start_node() 
-    {
-        ExpNode start_state = new ExpNode(NodeType.START_STATE, "START_NODE");
-        grammarTree.add(start_state);
-    }
-    
     // bracket handling
     void parse_bracket()
-    {   
-        ArrayList<ExpNode> tmpStrInBracket = new ArrayList<ExpNode> ();
-        // skip the open bracket
+    {
+        // jump to next char after the open bracket
         next_char();
-        String current_char = regexSeq.get(char_index);
+        current_char = regexSeq.get(char_index);
+        mustbe_character(current_char);
 
         // until close bracket 
-        while (!(current_char.equals(")")))
+        while (!((regexSeq.get(char_index)).equals(")")))
         {
-            mustbe_character(current_char);
+            current_char = regexSeq.get(char_index);
+            System.out.println(current_char);
             parse_character(current_char, tmpStrInBracket);
 
             if (current_char.equals("|"))
@@ -91,13 +86,15 @@ public class Parser {
                     System.exit(1);
                 }
             }
+            next_char();
         }   
 
         // when close bracket read
-        if (current_char.equals(")"))
-        {   
+        if ((regexSeq.get(char_index)).equals(")"))
+        {
             // skip close bracket and read if there "*" or "+" after bracket
             next_char();
+            current_char = regexSeq.get(char_index);
             if (current_char.equals("*"))
             {   
                 ExpNode newKleeneStarNode = expNodeCreator.create_zeroOrMore_node(current_char);
@@ -145,8 +142,6 @@ public class Parser {
             ExpNode newKleeneStarNode = expNodeCreator.create_zeroOrMore_node(current_char);
             newKleeneStarNode.children.add(newCharNode);
             targeted_list.add(newKleeneStarNode);
-            // skip the star
-            next_char();
 
         // // if kleeneplus is found, create a kleeneplus to which add the character node as a child
         } else if (have_kleenPlus()) 
@@ -156,8 +151,6 @@ public class Parser {
             ExpNode newKleenePlusNode = expNodeCreator.create_oneOrMore_node(current_char);
             newKleenePlusNode.children.add(newCharNode);
             targeted_list.add(newKleenePlusNode);
-            // skip the plus
-            next_char();
 
         } else
         {   
@@ -169,22 +162,22 @@ public class Parser {
     void parse_regex(ArrayList<String> regex) 
     {   
         System.out.println("test");
+        // store the regex input from RegexEngine
         regexSeq = regex;
         // for (int i = 0; i < regexSeq.size(); i++)
         // {
         //     System.out.printf("%s ", regexSeq.get(i));
         // }   
         while (char_index < regexSeq.size())
-        {   
+        {
             current_char = regexSeq.get(char_index);
             if (char_index == 0)
             {
-                create_start_node();
                 mustbe_characterOrOpenBracket(current_char);
             }
             
             if (current_char.equals("("))
-            {   
+            {
                 parse_bracket();
 
             } else if (current_char.equals("|"))
@@ -199,6 +192,10 @@ public class Parser {
 
         for (int i = 0; i < grammarTree.size(); i++)
         {
+            if (grammarTree.get(i).exp.equals("*") || grammarTree.get(i).exp.equals("+"))
+            {
+                System.out.printf("%s ", grammarTree.get(i).children);
+            }
             System.out.printf("%s ", grammarTree.get(i).exp);
         }
     }
